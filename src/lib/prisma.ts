bd2086d69623f7f84f73f6eafa8f4d7e56dd7177
Sushma Thapa <sushmat952@gmail.com>
2026-01-16 13:45:48 +0545

    prisma change for deployment

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,2 +1,8 @@
-// Prisma disabled for Cloudflare Pages build
-export {};
+// src/lib/prisma.ts
+// Stub for Cloudflare Pages deployment
+// This prevents build errors since Prisma can't run on Cloudflare Pages
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const prisma = {} as any;
+
+export default prisma;
