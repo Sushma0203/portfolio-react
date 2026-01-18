b874a5324677bcc23b732b3666751517aa4bcb56
Sushma Thapa <sushmat952@gmail.com>
2026-01-18 13:33:32 +0545

    chore: remove .vercel from git and ignore it

--- .vercel/output/static/_worker.js/__next-on-pages-dist__/manifest/__RSC_SERVER_MANIFEST.js
+++
@@ -1,5 +0,0 @@
-var n={},t=(e,_,o)=>(n.__RSC_SERVER_MANIFEST=e.__RSC_SERVER_MANIFEST=`{
-  "node": {},
-  "edge": {},
-  "encryptionKey": "7CMPyWwNq4VpahZ29KoTo+RTPFJ3XnuUZtmWeTn+8ws="
-}`,n);export{t as __getNamedExports};
